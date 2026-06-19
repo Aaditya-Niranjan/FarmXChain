5a0e1bd7096f0c44bcff3aecc9550596cec1312d
Aaditya Niranjan <adityaniranjan7@gmail.com>
2026-06-19 14:01:44 +0530

    Enable JWT authentication in Swagger

--- FarmXChain/src/main/java/com/infosys/farmxchain/config/OpenApiConfig.java
+++ FarmXChain/src/main/java/com/infosys/farmxchain/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.infosys.farmxchain.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +14,35 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .openapi("3.0.1")
                 .info(new Info()
                         .title("FarmXChain API")
                         .version("1.0")
                         .description("API documentation for FarmXChain application"))
-                .addServersItem(new Server().url("https://farmxchain-1.onrender.com/api/v1"));
+
+                .addServersItem(
+                        new Server()
+                                .url("https://farmxchain-1.onrender.com/api/v1")
+                )
+
+                .addSecurityItem(
+                        new SecurityRequirement().addList(securitySchemeName)
+                )
+
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
 }
